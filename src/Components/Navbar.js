0c365baa0ed28ecba811f958f5647b7469d3b436
rhmnaulia <rahmanauliax@gmail.com>
2020-09-16 20:27:20 +0700

    create card section

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
          <nav className="navbar">
             <div className="navbar-container">
                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                  TRVL
+                  TRVLX
                   <i class="fas fa-spa" />
                </Link>
                <div className="menu-icon" onClick={handleClick}>
