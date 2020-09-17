5df86291e26a0f618b947631624dcddd6c68e452
rhmnaulia <rahmanauliax@gmail.com>
2020-09-17 14:10:14 +0700

    fixing navbar hyperlink

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/pages/Home";
+import Products from "./Components/pages/Products";
+import Services from "./Components/pages/Services";
+import SignUp from "./Components/pages/SignUp";
 
 function App() {
    return (
@@ -11,6 +14,9 @@ function App() {
             <Navbar />
             <Switch>
                <Route path="/" exact component={Home} />
+               <Route path="/products" exact component={Products} />
+               <Route path="/services" exact component={Services} />
+               <Route path="/sign-up" exact component={SignUp} />
             </Switch>
          </Router>
       </>
