ddb9191837d9eb9bda0547f9299d32a124545988
rhmnaulia <rahmanauliax@gmail.com>
2020-10-08 20:18:30 +0700

    fixing some ambigious syntax

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
             <div className="navbar-container">
                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                   TRVLX
-                  <i class="fas fa-spa" />
+                  <i className="fas fa-spa" />
                </Link>
                <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? "fas fa-times" : "fas fa-bars"} />
